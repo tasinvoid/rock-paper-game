7921966cc59f2466fe56431238e567651e7080bf
tasin <shoaibmahmud4000@gmail.com>
2024-11-11 23:50:36 +0600

    playRound working

--- js.js
+++ js.js
@@ -1,4 +1,4 @@
-
+let humanScore =0,computerScore = 0;
 function getComputerChoice(){
     let randomNmber = Math.floor((Math.random()*3)+1)
     let computerChoice
@@ -18,5 +18,33 @@ function getComputerChoice(){
 }
 function getHumanChoice(){
     let humanChoice = prompt("Choose rock or paper or scissors").toUpperCase();
+    return humanChoice;
 }
-getHumanChoice()
+function playRound(humanChoice,computerChoice){
+    if(humanChoice == computerChoice){
+        console.log("it's a draw")
+    }
+    else if(humanChoice == "ROCK" && computerChoice == "PAPER"){
+        console.log("computerChoice =PAPER ,computer win")
+        computerScore++
+    }
+    else if(humanChoice == "PAPER" && computerChoice == "SCISSORS"){
+        console.log("computerChoice =SCISSORS ,computer win")
+        computerScore++
+
+    }
+    else if(humanChoice == "SCISSORS" && computerChoice == "ROCK"){
+        console.log("computerChoice =ROCK ,computer win")
+        computerScore++
+
+    }
+    else{
+        console.log("you win");
+        humanScore++
+    }
+
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+playRound(humanSelection, computerSelection);
