01b1b577b85ae588cc863a34ac8dd3fd39fa68ee
tasin <shoaibmahmud4000@gmail.com>
2024-11-11 22:47:18 +0600

    getComputerChoice working

--- js.js
+++ js.js
@@ -1,1 +1,19 @@
-console.log("hello ")
+
+function getComputerChoice(){
+    let randomNmber = Math.floor((Math.random()*3)+1)
+    let computerChoice
+    switch (randomNmber) {
+        case 1:
+            computerChoice = "ROCK";
+            break;
+        case 2:
+            computerChoice = "PAPER";
+            break;
+        case 3:
+            computerChoice = "SCISSORS";
+            break;
+    }
+    return computerChoice
+    
+}
+console.log(getComputerChoice())
